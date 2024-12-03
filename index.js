aebc9473794bfddf075b0428acc34cc4746de774
Mrd7m4654776 <mrd7m888@gmail.com>
2024-12-03 03:01:16 +0300

    Update index.js

--- index.js
+++ index.js
@@ -38,10 +38,10 @@ client.on('ready', async () => {
     .setURL('https://www.twitch.tv/') 
     .setState('أما بفعل يمناك ولا تبرقع')
     .setName('Mr.D7m')
-    .setDetails(`.`)
+    .setDetails(`Mr.d7m`)
     .setStartTimestamp(Date.now())
  .setAssetsLargeImage('https://media.discordapp.net/attachments/1311069774293237813/1313292450764427334/ba2a8222ebddc79a055a0d6be0421b76.jpg?ex=674f9a68&is=674e48e8&hm=b8de94f9f396d74931f5a21c972cc323d887831a8ab0bdd94a73cdb4447193c1&') //You can put links in tenor or discord and etc.
-    .setAssetsLargeText('-') 
+    .setAssetsLargeText('# Staff , Developer Discord ') 
     .setAssetsSmallImage('https://media.discordapp.net/attachments/1311069774293237813/1313292928021434439/1313292840784367682remix-1733183831189.png?ex=674f9ad9&is=674e4959&hm=6ceb75ad82cbdbf27c4e9b660290780f075d490b949bb19f6bdd462a4df6d0fb&') //You can put links in tenor or discord and etc.
     .setAssetsSmallText('Small Text') 
     .addButton('DISCORD', 'https://discord.gg/');
