71ea941f2fbeb542c21c8a217b1e5bbb5fcfdbcc
Mrd7m4654776 <mrd7m888@gmail.com>
2024-12-12 01:07:19 +0300

    Update index.js

--- index.js
+++ index.js
@@ -36,15 +36,15 @@ client.on('ready', async () => {
     .setApplicationId('1257088531843649740')
     .setType('PLAYING')
     .setURL('https://www.twitch.tv/') 
-    .setState('d7oom')
-    .setName('سيرفر مبهر للجميع 🛠️')
-    .setDetails(`حياكم الله يالنشامى`)
+    .setState('🛠️｜HighStaff')
+    .setName('🔗｜Welcome to the s5b')
+    .setDetails(`📜｜Law is respect`)
     .setStartTimestamp(Date.now())
  .setAssetsLargeImage('https://media.discordapp.net/attachments/1014189787395133511/1014308561968435260/LogoDiscordLoading.gif?ex=67577676&is=675624f6&hm=b09d406c214511b61ebb7ddcb13f075c592de6b8a99689d2ed18a4aee83a8eed&') //You can put links in tenor or discord and etc.
     .setAssetsLargeText('# S5B | HighStaff') 
     .setAssetsSmallImage('https://media.discordapp.net/attachments/1014189787395133511/1014308561968435260/LogoDiscordLoading.gif?ex=67577676&is=675624f6&hm=b09d406c214511b61ebb7ddcb13f075c592de6b8a99689d2ed18a4aee83a8eed&') //You can put links in tenor or discord and etc.
     .setAssetsSmallText('Small Text') 
-    .addButton('Server S5B', 'https://discord.gg/s5b');
+    .addButton('SERVER S5B', 'https://discord.gg/s5b');
 
   client.user.setActivity(r);
   client.user.setPresence({ status: "dnd" }); //dnd, online, idle, offline
