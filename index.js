80295b90f46d9219461489a73cd19c3746e76064
Mrd7m4654776 <mrd7m888@gmail.com>
2024-12-12 20:06:41 +0300

    Update index.js

--- index.js
+++ index.js
@@ -41,7 +41,7 @@ client.on('ready', async () => {
     .setDetails(`🍂・تثقف في الدين الاسلامي`)
     .setStartTimestamp(Date.now())
  .setAssetsLargeImage('https://media.discordapp.net/attachments/1270151141660819538/1316808017345380373/7424B268-2B32-4F2E-8AD2-0D9F58942942.png?ex=675c6488&is=675b1308&hm=d63a5e633afe115f8b0e1ab263aa68415923cdf052dc345d1755358676b484dc&') //You can put links in tenor or discord and etc.
-    .setAssetsLargeText('🤍・أَلا بِذِكْرِ اللَّهِ تَطْمَئِنُّ الْقُلُوبُ') 
+    .setAssetsLargeText('🤍・ألا بذكر الله تطمئن القلوب') 
     .setAssetsSmallImage('https://media.discordapp.net/attachments/1270151141660819538/1316808017345380373/7424B268-2B32-4F2E-8AD2-0D9F58942942.png?ex=675c6488&is=675b1308&hm=d63a5e633afe115f8b0e1ab263aa68415923cdf052dc345d1755358676b484dc&') //You can put links in tenor or discord and etc.
     .setAssetsSmallText('Small Text') 
     .addButton('سيرفر لحَّظٰت', 'https://discord.gg/5G9eGxsw');
