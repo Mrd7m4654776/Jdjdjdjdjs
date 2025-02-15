14fd112108f6904cb84ebd5a7c11db6e35affcf3
Mrd7m4654776 <mrd7m888@gmail.com>
2025-02-15 14:20:04 +0300

    Update index.js

--- index.js
+++ index.js
@@ -36,13 +36,13 @@ client.on('ready', async () => {
     .setApplicationId('778357315459874826')
     .setType('PLAYING')
     .setURL('https://www.twitch.tv/') 
-    .setState('-')
+    .setState('mr')
     .setName('Abdulrahman Bin Ali')
-    .setDetails(`-`)
+    .setDetails(`guerriero di successo`)
     .setStartTimestamp(Date.now())
  .setAssetsLargeImage('https://media.discordapp.net/attachments/1337039991242756157/1340279629625298974/45b0ac938a298610c0fcb13028056fe6.jpg?ex=67b1c82c&is=67b076ac&hm=b74af2ff37c13a20df3c1922007ec92e25f50e9dab0947bf404a4193d86c0de7&') //You can put links in tenor or discord and etc.
     .setAssetsLargeText('🛠️') 
-    .setAssetsSmallImage('https://media.discordapp.net/attachments/1317934667126538391/1317936004346417263/Picsart_24-12-15_22-26-56-469.png?ex=67607f0d&is=675f2d8d&hm=78adfed7e485e7ead3c881637bdd1747d323c123dfead98dfc3654f7749c44c7&') //You can put links in tenor or discord and etc.
+    .setAssetsSmallImage('https://media.discordapp.net/attachments/1337039991242756157/1340279629625298974/45b0ac938a298610c0fcb13028056fe6.jpg?ex=67b1c82c&is=67b076ac&hm=b74af2ff37c13a20df3c1922007ec92e25f50e9dab0947bf404a4193d86c0de7&') //You can put links in tenor or discord and etc.
     .setAssetsSmallText('Small Text') 
     
 
